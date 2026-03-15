1e6ef348862aaf18386c77862ca3f20943173fc3
Mohamed Aziz Ben Hlel <tigana137@gmail.com>
2026-03-15 11:34:11 +0100

    added firebase config values to the env

--- apps/admin/src/config/env.variables.ts
+++ apps/admin/src/config/env.variables.ts
@@ -3,20 +3,26 @@ import { z } from 'zod';
 const ENV_schema = z
   .object({
     VITE_NODE_ENV: z.enum(['dev', 'stage', 'build', 'production']),
-    VITE_PORT: z.coerce.number().int().positive().optional(),
+    VITE_ADMIN_PORT: z.coerce.number().int().positive().optional(),
     VITE_API_URL: z.string(),
+    VITE_FIREBASE_API_KEY: z.string(),
+    VITE_FIREBASE_AUTH_DOMAIN: z.string(),
+    VITE_FIREBASE_PROJECT_ID: z.string(),
+    VITE_FIREBASE_STORAGE_BUCKET: z.string(),
+    VITE_FIREBASE_MESSAGING_SENDER_ID: z.string(),
+    VITE_FIREBASE_APP_ID: z.string(),
+    VITE_FIREBASE_MEASUREMENT_ID: z.string(),
   })
   .refine(
     (data) => {
-      // If env is not dev/test, then web port must exist
-      if (['dev'].includes(data.VITE_NODE_ENV)) {
-        return data.VITE_PORT !== undefined;
+      if (['dev', 'build'].includes(data.VITE_NODE_ENV)) {
+        return data.VITE_ADMIN_PORT !== undefined;
       }
       return true;
     },
     {
-      path: ['VITE_WEB_PORT'], // points error to the right field
-      message: 'VITE_WEB_PORT is required in development or test environments',
+      path: ['VITE_ADMIN_PORT'],
+      message: 'VITE_ADMIN_PORT is required when NODE_ENV is dev or build',
     },
   );
 
--- apps/admin/src/config/firebase.ts
+++ apps/admin/src/config/firebase.ts
@@ -2,19 +2,20 @@
 import { getApps, initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 import { getAuth } from 'firebase/auth';
+import ENV from './env.variables';
 // ! fix this it s wrong it can get you this erro in production :
 // Uncaught FirebaseError: Firebase: No Firebase App '[DEFAULT]' has been created - call initializeApp() first (app/no-app).
 // you just copy pased the fix from chat but this is not quite good , you re doing the same logic twice it s like a race condition thing,
 // both racin to init first, you prob already need to change the whol imp in the first place
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyDWKplAl5Vp43pG0j5j3vnThRJIPwv1E44',
-  authDomain: 'uber-588cf.firebaseapp.com',
-  projectId: 'uber-588cf',
-  storageBucket: 'uber-588cf.firebasestorage.app',
-  messagingSenderId: '371351638566',
-  appId: '1:371351638566:web:16c040ae9fb03d5382cce7',
-  measurementId: 'G-DH71YEDZKQ',
+  apiKey: ENV.VITE_FIREBASE_API_KEY,
+  authDomain: ENV.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: ENV.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: ENV.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: ENV.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: ENV.VITE_FIREBASE_APP_ID,
+  measurementId: ENV.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase app only once
