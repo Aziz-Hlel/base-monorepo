a19a6fc37b93b0b702627fc5415698a5c9f6b47a
Mohamed Aziz Ben Hlel <tigana137@gmail.com>
2026-03-15 14:37:59 +0100

    fixed cors

--- apps/api/src/config/ENV.ts
+++ apps/api/src/config/ENV.ts
@@ -1,64 +1,80 @@
 import dotenv from 'dotenv';
 import z from 'zod';
+import { devEnvs, prodEnvs } from './NodeEnvs';
 
 dotenv.config();
 
-const baseSchema = z
-  .object({
-    // APP
-    API_PORT: z.coerce.number().positive(),
+const baseSchema = z.object({
+  // APP
+  API_PORT: z.coerce.number().positive(),
 
-    // CORS
-    ALLOWED_ORIGIN_PATTERNS: z.string({ error: 'ALLOWED_ORIGIN_PATTERNS is required in non production environment' }),
+  // FIREBASE
+  FIREBASE_CERT: z.string().min(1),
 
-    // FIREBASE
-    FIREBASE_CERT: z.string().min(1),
+  // DB
+  DB_USER: z.string(),
+  DB_PASSWORD: z.string(),
+  DB_NAME: z.string(),
+  DB_PORT: z.coerce.number(),
+  DB_HOST: z.enum(['localhost', 'db']),
 
-    // DB
-    DB_USER: z.string(),
-    DB_PASSWORD: z.string(),
-    DB_NAME: z.string(),
-    DB_PORT: z.coerce.number(),
-    DB_HOST: z.enum(['localhost', 'db']),
+  // REDIS
+  REDIS_PORT: z.coerce.number().positive(),
+  REDIS_PASSWORD: z.string().min(1),
+  REDIS_HOST: z.enum(['localhost', 'redis']),
 
-    // REDIS
-    REDIS_PORT: z.coerce.number().positive(),
-    REDIS_PASSWORD: z.string().min(1),
-    REDIS_HOST: z.enum(['localhost', 'redis']),
+  // SMTP
+  SMTP_HOST: z.string(),
+  SMTP_PORT: z.coerce.number().positive(),
+  SMTP_SECURE: z.string().transform((val) => val === 'true'),
+  SMTP_USER: z.string(),
+  SMTP_PASS: z.string(),
 
-    // SMTP
-    SMTP_HOST: z.string(),
-    SMTP_PORT: z.coerce.number().positive(),
-    SMTP_SECURE: z.string().transform((val) => val === 'true'),
-    SMTP_USER: z.string(),
-    SMTP_PASS: z.string(),
-
-    // APP VERSIONS
-    IOS_MIN_SUPPORTED_VER: z.string().regex(/^\d+\.\d+\.\d+$/, {
-      message: 'IOS_MIN_SUPPORTED_VER must be in the format x.y.z',
-    }),
-    ANDROID_MIN_SUPPORTED_VER: z.string().regex(/^\d+\.\d+\.\d+$/, {
-      message: 'ANDROID_MIN_SUPPORTED_VER must be in the format x.y.z',
-    }),
-  })
-  .refine(
-    (data) => {
-      try {
-        new RegExp(data.ALLOWED_ORIGIN_PATTERNS);
-        return true;
-      } catch (_) {
-        return false;
-      }
-    },
-    { error: 'ALLOWED_ORIGIN_PATTERNS is invalid' },
-  );
+  // APP VERSIONS
+  IOS_MIN_SUPPORTED_VER: z.string().regex(/^\d+\.\d+\.\d+$/, {
+    message: 'IOS_MIN_SUPPORTED_VER must be in the format x.y.z',
+  }),
+  ANDROID_MIN_SUPPORTED_VER: z.string().regex(/^\d+\.\d+\.\d+$/, {
+    message: 'ANDROID_MIN_SUPPORTED_VER must be in the format x.y.z',
+  }),
+});
 
 const prodSchema = baseSchema.extend({
-  NODE_ENV: z.enum(['production', 'stage']),
+  NODE_ENV: z.enum(prodEnvs),
+
+  // STORAGE
+  AWS_REGION: z.string(),
+  AWS_ACCESS_KEY_ID: z.string(),
+  AWS_SECRET_ACCESS_KEY: z.string(),
+  AWS_S3_BUCKET: z.string(),
+
+  // CDN
+  AWS_CLOUDFRONT_URL: z.string(),
+
+  // CORS
+  ALLOWED_ORIGIN_PATTERNS: z
+    .string({ error: 'ALLOWED_ORIGIN_PATTERNS is required in prod environments' })
+    .refine(
+      (origins) => {
+        try {
+          const regexOrigins = /^https:\/\/([a-z0-9-]+\.)+[a-z]{2,}(,([a-z0-9-]+\.)+[a-z]{2,})*$/i;
+          return regexOrigins.test(origins);
+        } catch (_) {
+          return false;
+        }
+      },
+      {
+        error:
+          'ALLOWED_ORIGIN_PATTERNS is invalid, it does not follow the pattern https://domain.com,https://domain.com',
+      },
+    )
+    .transform((origins) => origins.split(',')),
 });
 
 const devSchema = baseSchema.extend({
-  NODE_ENV: z.enum(['dev', 'build']),
+  NODE_ENV: z.enum(devEnvs),
+
+  // STORAGE
   MINIO_REGION: z.string(),
   MINIO_ROOT_USER: z.string(),
   MINIO_ROOT_PASSWORD: z.string(),
--- apps/api/src/config/cors.ts
+++ apps/api/src/config/cors.ts
@@ -1,9 +1,10 @@
 import cors from 'cors';
 import ENV from './ENV';
+import { isProdEnv } from './NodeEnvs';
 
 export function configureCors() {
   return cors({
-    origin: new RegExp(ENV.ALLOWED_ORIGIN_PATTERNS || '.*'),
+    origin: isProdEnv(ENV) ? ENV.ALLOWED_ORIGIN_PATTERNS : '.*',
 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
