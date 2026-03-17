9fa9578b17925275a8073e7485326052c9582546
Mohamed Aziz Ben Hlel <tigana137@gmail.com>
2026-03-17 20:03:31 +0100

    ditched the generic prisma repo idea

--- apps/api/src/products/product.controller.ts
+++ apps/api/src/products/product.controller.ts
@@ -3,7 +3,6 @@ import { productService } from './product.service';
 import { Request, Response } from 'express';
 import { ProductResponse } from '@repo/contracts/schemas/product/productResponse';
 import { updateProductRequestSchema } from '@repo/contracts/schemas/product/updateProductRequest';
-import { SimpleApiResponse } from '@repo/contracts/types/api/SimpleApiResponse.dto';
 import { productsQueryParamsSchema } from '@repo/contracts/schemas/product/ProductPageQuery';
 import getParam from '@/utils/getParam';
 
