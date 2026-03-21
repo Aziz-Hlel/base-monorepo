b2dd4e5b9b5f6b4d68b5a1018559079ca212cb0a
Mohamed Aziz Ben Hlel <tigana137@gmail.com>
2026-03-21 23:46:08 +0100

    still switchin to stricter OOP strcuture

--- apps/api/src/container/index.ts
+++ apps/api/src/container/index.ts
@@ -3,13 +3,17 @@ import { createProductModule } from '@/modules/products';
 import { createRootModule } from '@/modules/root';
 import { createMediaModule } from '@/media';
 
+// * ROOT
 const { rootRouter } = createRootModule();
 
+// * MEDIA
 const { mediaRouter, mediaService } = createMediaModule();
 
+// * USER
 const { userRouter, userInternalService } = createUserModule();
 const { authRouter } = createAuthModule(userInternalService);
 
+// * PRODUCT
 const { productRouter } = createProductModule(mediaService);
 
 export { rootRouter, userRouter, authRouter, productRouter, mediaRouter, mediaService };
